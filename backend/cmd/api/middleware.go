85c730bc183035e8e6fb51eceaed5c4b88738017
bradpreston <bap5393@gmail.com>
2022-07-21 17:05:37 -0400

    added a cors function to allow API access from any url

---
+++ backend/cmd/api/middleware.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+// enableCors allows access to our API from any url
+func (app * application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		next.ServeHTTP(w, r)
+	})
+}
