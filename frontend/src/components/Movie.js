b5f6c713f496f9c7ddbf6b8abb079ed6661d9b38
bradpreston <bap5393@gmail.com>
2022-07-21 18:33:59 -0400

    Added fetch request for all movies and single movies from front-end to API

--- frontend/src/components/Movie.js
+++ frontend/src/components/Movie.js
@@ -2,38 +2,81 @@ import React, { useState, useEffect } from "react"
 
 const Movie = (props) => {
 	const [movie, setMovie] = useState({})
+	const [isLoaded, setIsLoaded] = useState(false)
+	const [error, setError] = useState(null)
 
 	useEffect(() => {
-		setMovie({
-			id: props.match.params.id,
-			title: "Some Movie",
-			runtime: 150,
-		})
+		(async function fetchMovie() {
+			const res = await fetch("http://localhost:8080/v1/movie/" + props.match.params.id)
+			if (res.status !== 200) {
+				const err = Error("Invalid response code: " + + res.status)
+				setError(err)
+				setIsLoaded(false)
+			} else {
+				const json = await res.json()
+				setMovie(json.movie)
+				setIsLoaded(true)
+			}
+		}())
 	}, [])
 
-	return (
-		<>
-			<h2>Movie: {movie.title}</h2>
+	if (movie.genres) {
+		movie.genres = Object.values(movie.genres)
+	} else {
+		movie.genres = [];
+	}
 
-			<table className="table table-compact table-striped">
-				<thead></thead>
-				<tbody>
-					<tr>
-						<td>
-							<strong>Title:</strong>
-						</td>
-						<td>{movie.title}</td>
-					</tr>
-					<tr>
-						<td>
-							<strong>Runtime:</strong>
-						</td>
-						<td>{movie.runtime}</td>
-					</tr>
-				</tbody>
-			</table>
-		</>
-	)
+	if (error) {
+		return <div><strong>{error.message}</strong></div>
+	}
+	else if (isLoaded) {
+		return (
+			<>
+				<h2>Movie: {movie.title} ({movie.year})</h2>
+
+				<div className="float-start">
+					<small>rating: {movie.mpaa_rating}</small>
+				</div>
+				<div className="float-end">
+					{movie.genres.map((movie, i) => (
+						<span className="badge bg-secondary me-1" key={i}>
+							{movie}
+						</span>
+					))}
+				</div>
+				<div className="clearfix"></div>
+				<hr />
+
+				<table className="table table-compact table-striped">
+					<thead></thead>
+					<tbody>
+						<tr>
+							<td>
+								<strong>Title:</strong>
+							</td>
+							<td>{movie.title}</td>
+						</tr>
+
+						<tr>
+							<td>
+								<strong>Description:</strong>
+							</td>
+							<td>{movie.description}</td>
+						</tr>
+
+						<tr>
+							<td>
+								<strong>Runtime:</strong>
+							</td>
+							<td>{movie.runtime} minutes</td>
+						</tr>
+					</tbody>
+				</table>
+			</>
+		)
+	} else {
+		return <p>Loading...</p>
+	}
 }
 
 export default Movie
--- frontend/src/components/Movies.js
+++ frontend/src/components/Movies.js
@@ -3,32 +3,48 @@ import { Link } from "react-router-dom"
 
 const Movies = () => {
 	const [movies, setMovies] = useState([])
+	const [isLoaded, setIsLoaded] = useState(false)
+	const [error, setError] = useState(null)
 
 	useEffect(() => {
-		setMovies([
-			{ id: 1, title: "Spiderman", runtime: 109 },
-			{ id: 2, title: "Logan", runtime: 142 },
-			{ id: 3, title: "The Princess Bride", runtime: 85 },
-			{ id: 4, title: "Soul", runtime: 97 },
-		])
+		(async function fetchMovies() {
+			const res = await fetch("http://localhost:8080/v1/movies")
+			if (res.status !== 200) {
+				const err = Error("Invalid response code: " + + res.status)
+				setError(err)
+				setIsLoaded(false)
+			} else {
+				const json = await res.json()
+				setMovies(json.movies)
+				setIsLoaded(true)
+			}
+		}())
 	}, [])
 
-	return (
-		<>
-			<h2>Choose a movie</h2>
-
-			<ul>
-				{movies &&
-					movies.map((movie) => {
-						return (
-							<li key={movie.id}>
-								<Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-							</li>
-						)
-					})}
-			</ul>
-		</>
-	)
+	if (error) {
+		return <div><strong>{error.message}</strong></div>
+	}
+	else if (isLoaded) {
+		return (
+			<>
+				<h2>Choose a movie</h2>
+	
+				<ul>
+					{movies &&
+						movies.map((movie) => {
+							return (
+								<li key={movie.id}>
+									<Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+								</li>
+							)
+						})}
+				</ul>
+			</>
+		)
+	} else {
+		return <p>Loading...</p>
+	}
+	
 }
 
 export default Movies
