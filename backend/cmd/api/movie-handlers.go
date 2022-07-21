06bd246ca99ccfe69b9b37b4cd1cce6de76a4d9d
bradpreston <bap5393@gmail.com>
2022-07-20 21:26:58 -0400

    created a function to GET a single movie from the database

--- backend/cmd/api/movie-handlers.go
+++ backend/cmd/api/movie-handlers.go
@@ -4,9 +4,7 @@ import (
 	"errors"
 	"net/http"
 	"strconv"
-	"time"
 
-	"github.com/BradPreston/go-movies/backend/models"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -20,17 +18,9 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	movie := models.Movie {
-		ID: id,
-		Title: "Some movie",
-		Description: "some description",
-		Year: 2021,
-		ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
-		Runtime: 100,
-		Rating: 5,
-		MPAARating: "PG-13",
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
+	movie, err := app.models.DB.Get(id)
+	if err != nil {
+		app.logger.Println(errors.New("could not get movie by id"))
 	}
 
 	if err = app.writeJSON(w, http.StatusOK, movie, "movie"); err != nil {
