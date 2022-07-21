ee1e7293896dcc9651a09e7caaf948704cdef140
bradpreston <bap5393@gmail.com>
2022-07-21 16:44:16 -0400

    created a function to GET all  movies from the database

--- backend/cmd/api/movie-handlers.go
+++ backend/cmd/api/movie-handlers.go
@@ -20,14 +20,25 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 
 	movie, err := app.models.DB.Get(id)
 	if err != nil {
-		app.logger.Println(errors.New("could not get movie by id"))
+		app.errorJSON(w, err)
+		return
 	}
 
 	if err = app.writeJSON(w, http.StatusOK, movie, "movie"); err != nil {
-		app.logger.Println(errors.New("could not write movie to json"))
+		app.errorJSON(w, err)
+		return
 	}
 }
 
 func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
-	
+	movies, err := app.models.DB.All()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	if err = app.writeJSON(w, http.StatusOK, movies, "movies"); err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
--- backend/models/models.go
+++ backend/models/models.go
@@ -29,7 +29,7 @@ type Movie struct {
 	MPAARating string `json:"mpaa_rating"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
-	MovieGenre []MovieGenre `json:"genres"`
+	MovieGenre map[int]string `json:"genres"`
 }
 
 // Genre is the type for a genre
