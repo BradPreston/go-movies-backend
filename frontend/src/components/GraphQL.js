0096e91414e8e36470967a60f907de85627c9a69
bradpreston <bap5393@gmail.com>
2022-07-28 16:38:18 -0400

    added search functionality to graphql

--- frontend/src/components/GraphQL.js
+++ frontend/src/components/GraphQL.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react"
+import Input from "./form-components/Input"
 
 export default function GraphQL(props) {
 	const [movies, setMovies] = useState([])
 	const [isLoaded, setIsLoaded] = useState(false)
 	const [error, setError] = useState(null)
 	const [alert, setAlert] = useState({ type: "d-none", message: "" })
+	const [searchTerm, setSearchTerm] = useState("")
 
 	useEffect(() => {
 		const payload = `
@@ -29,19 +31,55 @@ export default function GraphQL(props) {
 		}
 
 		async function fetchMovies() {
-			const res = await fetch("http://localhost:8080/v1/graphql/list", options)
+			const res = await fetch("http://localhost:8080/v1/graphql", options)
 			const data = await res.json()
 			const movieList = Object.values(data.data.list)
 			setMovies(movieList)
 		}
 
 		fetchMovies()
-	})
+
+		handleSearch(searchTerm)
+	}, [searchTerm])
+
+	function handleSearch(searchTerm) {
+		const payload = `
+        {
+            search(titleContains: "${searchTerm}") {
+                id
+                title
+                runtime
+                year
+                description
+            }
+        }
+        `
+
+		const headers = new Headers()
+		headers.append("Content-Type", "application/json")
+
+		const options = {
+			method: "POST",
+			body: payload,
+			headers: headers,
+		}
+
+		async function fetchMovies() {
+			const res = await fetch("http://localhost:8080/v1/graphql", options)
+			const data = await res.json()
+			const movieList = Object.values(data.data.search)
+			if (movieList.length > 0) setMovies(movieList)
+			else setMovies([])
+		}
+
+		fetchMovies()
+	}
 
 	return (
 		<>
 			<h2>GraphQL</h2>
 			<hr />
+			<Input title="search" type="text" name="search" value={searchTerm} setValue={(e) => setSearchTerm(e.target.value)} placeholder="Search by movie title" />
 			<div className="list-group">
 				{movies.map((movie) => (
 					<a key={movie.id} className="list-group-item list-group-item-aciton" href="#!">
